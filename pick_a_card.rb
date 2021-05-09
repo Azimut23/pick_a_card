288c5f8326c6bb4d1e0c6e5f43e98305aa0ee16f
Azimut23 <gmforex01@mail.ru>
2021-05-09 21:19:50 +0500

    update pick_a_card v.2

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,4 +2,4 @@
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 suits = ['Черви', 'Пики', 'Крести', 'Буби']
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample} #{suits.sample}"
