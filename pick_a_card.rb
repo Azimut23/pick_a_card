1a119bbbef5ff250f14eba4f86103fdca287b4e5
Azimut23 <gmforex01@mail.ru>
2021-05-12 18:27:28 +0500

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-suits = ['Черви', 'Пики', 'Крести', 'Буби']
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+suits = %w[♠ ♥ ♦ ♣]
 
 puts "#{values.sample} #{suits.sample}"
